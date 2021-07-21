9496d1ec6bdb377622b0f8d54fd10a8040358cc8
Ciaran Concannon <cjcon90@pm.me>
2021-07-21 23:22:40 +0100

    add picom, alacritty, i3, polybar configs

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Reload Siji Glyphs
+fc-cache -f
+# Launch bar1 and bar2
+DISPLAY1="$(xrandr -q | grep 'eDP-1' | cut -d ' ' -f1)"
+[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar main &
+
+DISPLAY2="$(xrandr -q | grep 'HDMI-2' | cut -d ' ' -f1)"
+[[ ! -z $DISPLAY2 ]] && MONITOR=$DISPLAY2 polybar main &
+
+echo "Bars launched..."
