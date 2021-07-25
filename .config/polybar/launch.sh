d33f06f3ba43cd5068a78b643d3a36d437bde6b2
Ciaran Concannon <cjcon90@pm.me>
2021-07-25 18:28:57 +0100

    changed polybar icons to fontawesome

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Reload Siji Glyphs
-fc-cache -f
 # Launch bar1 and bar2
 DISPLAY1="$(xrandr -q | grep 'eDP-1' | cut -d ' ' -f1)"
 [[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar main &
