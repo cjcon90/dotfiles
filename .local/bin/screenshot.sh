ba2e5ed363ec27b5a33583315ea850532db44af6
Ciaran Concannon <cjcon90@pm.me>
2023-08-15 10:53:46 +0100

    Use mako for notifs / Grim for screenshot

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-folder=~/Pictures/screenshots
+SCREENSHOT_URL=Pictures/screenshots/"$(date "+%y%m%d%H%M%S")"_full.png
 
-grim "$folder"/"$(date +%Y%m%d-%H%M%S)".png
+grim $SCREENSHOT_URL
+
+notify-send "Screenshot Captured!" "Saved to: $SCREENSHOT_URL" --icon=dialog-information
--- .local/bin/screenshot_select.sh
+++ .local/bin/screenshot_select.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-folder=~/Pictures/screenshots
+SCREENSHOT_URL=Pictures/screenshots/"$(date "+%y%m%d%H%M%S")"_select.png
 
-grim -g "$(slurp)" "$folder"/"$(date +%Y%m%d-%H%M%S)".png
+grim -g "$(slurp)" $SCREENSHOT_URL
+
+notify-send "Screenshot Captured!" "Saved to: $SCREENSHOT_URL" --icon=dialog-information
