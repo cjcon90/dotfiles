c6206e0521c7a1de2f36f0d8fbf029900dd739de
Ciaran Concannon <cjcon90@pm.me>
2024-01-02 22:52:58 +0000

    Add lsp key mappings

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,34 +1,26 @@
--- vim.keymap.set(
---    "n",
---    "<Leader>e",
---    vim.diagnostic.open_float,
---    { noremap = true, silent = true }
--- )
--- vim.keymap.set(
---    "n",
---    "<Leader>a",
---    vim.lsp.buf.code_action,
---    { noremap = true, silent = true }
--- )
---
--- vim.keymap.set("n", "<space>d", vim.diagnostic.open_float)
--- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
--- vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
---
--- vim.api.nvim_create_autocmd("LspAttach", {
---    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
---    callback = function(ev)
---       vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
---
---       local opts = { buffer = ev.buf }
---       vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
---       vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
---       vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
---       vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
---       -- vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
---       vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
---    end,
--- })
+vim.keymap.set(
+   "n",
+   "<Leader>a",
+   vim.lsp.buf.code_action,
+   { noremap = true, silent = true }
+)
+
+vim.keymap.set("n", "<space>d", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+
+vim.api.nvim_create_autocmd("LspAttach", {
+   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+   callback = function(ev)
+      vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+      local opts = { buffer = ev.buf }
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+   end,
+})
 
 local languages = {
    "clangd",
