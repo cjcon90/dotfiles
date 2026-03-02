b324596fdbcbb3ca5af96863dc01abacd4266eea
Ciaran Concannon <cjcon90@pm.me>
2026-03-02 23:29:58 +0000

    update waybar & sway configs

--- .config/sway/swap-displays.sh
+++ .config/sway/swap-displays.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# Listing all workspaces but the first and move them to external output
-# swaymsg -rt get_workspaces | jq '.[]|select(.num != 1).num' | xargs -I {} swaymsg workspace number {}, move workspace to output $external_output
-#!/usr/bin/env bash# Getting the new output nameexternal_output=$(swaymsg -t get_outputs | jq '.[].name | select(. != "eDP-1")')# Listing all workspaces but the first and move them to external outputswaymsg -rt get_workspaces | jq '.[]|select(.num != 1).num' | xargs -I {} swaymsg workspace number {}, move workspace to output $external_output
 
 # Getting the external output name
 external_output=$(swaymsg -t get_outputs | jq '.[].name | select(. != "eDP-1")')
@@ -18,18 +15,16 @@ visible=$(swaymsg -t get_workspaces | jq '.[] | select(.visible == true) | selec
 # Get currently focused workspace
 focused=$(swaymsg -t get_workspaces | jq '.[] | select(.focused == true) | .name')
 
-# # Move eDP-1 workspaces to external
+# Move eDP-1 workspaces to external
 for workspace in $internal_workspaces; do
     swaymsg "workspace $workspace, move workspace to output $external_output"
 done
 
-# # Move external workspaces to eDP-1
+# Move external workspaces to eDP-1
 for workspace in $external_workspaces; do
     swaymsg "workspace $workspace, move workspace to output eDP-1"
 done
 
-# Normally after swapping displays we want currently focused to be on other screen, 
-# and visible workspace from other screen to be focused
+# Restore focus: focused on other screen, visible from other screen becomes focused
 swaymsg workspace $focused
 swaymsg workspace $visible
-
--- .config/sway/swaynag.sh
+++ .config/sway/swaynag.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 
-swaynag -c $HOME/.config/swaynag/config -t warning -m 'Power Menu Options' -b 'Logout' 'swaymsg exit' -b 'Suspend' 'sudo systemctl suspend' -b 'Reboot' 'sudo systemctl reboot' -b 'Shutdown' 'sudo systemctl poweroff'
+swaynag -c $HOME/.config/swaynag/config -t warning -m 'Power Menu Options' \
+  -b 'Logout' 'swaymsg exit' \
+  -b 'Suspend' '$HOME/.config/sway/swaylock-blur.sh && systemctl suspend' \
+  -b 'Reboot' 'systemctl reboot' \
+  -b 'Shutdown' 'systemctl poweroff'
